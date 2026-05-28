25d34e693256ec75ed04e4dda4d094f8755ecbbe
Carlos Sampol <sampol.90@gmail.com>
2026-05-28 09:36:42 -0500

    feat: migrate api routes from next.js to express for extensibility

--- src/lib/pinata.js
+++
@@ -1,8 +0,0 @@
-// utils/config.ts
-// server only
-import { PinataSDK } from "pinata";
-
-export const pinata = new PinataSDK({
-  pinataJwt: process.env.PINATA_JWT,
-  pinataGateway: process.env.NEXT_PUBLIC_GATEWAY_URL,
-});
